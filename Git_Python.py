e3e8ec25a596e2072842e13aefe161949c659580
danielarnarsson <danielar216@nemi.tskoli.is>
2017-01-29 23:39:25 +0000

    Klaradi spurningu 1

---
+++ Git_Python.py
@@ -0,0 +1,7 @@
+#Daemi 1
+tala1=int(input("Sláðu inn tölu "))
+tala2=int(input("Sláðu inn aðra tölu "))
+samlagning=tala1+tala2
+print("Tölurnar lagðar saman: ", samlagning)
+margfoldun=tala1*tala2
+print("Tölurnar margfaldaðar saman: ", margfoldun)
